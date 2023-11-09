40b3decf44194a16b01a03307da0631e29959c9e
duyhieufpt123 <dumaconcacditmemay@gmail.com>
2023-11-10 04:15:20 +0700

    ádas

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -4,7 +4,7 @@ const roleCheck = require('../middleware/roleCheck');
 const ticketController = require('../controllers/ticketController');
 const router = express.Router();
 
-router.get('/', auth, roleCheck('admin'), ticketController.getAllTickets);
+router.get('/', auth, ticketController.getAllTickets);
 router.get('/:id', auth, roleCheck('admin'), ticketController.getTicketById);
 router.post('/', auth, roleCheck('admin'), ticketController.createTicket);
 router.put('/:id', auth, roleCheck('admin'), ticketController.updateTicket);
